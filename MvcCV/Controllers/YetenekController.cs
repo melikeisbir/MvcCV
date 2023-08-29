250ba6dafc6272828f636a0a57025278de438fba
Melike İsbir <melikeisbr@gmail.com>
2023-08-29 20:21:39 +0300

    color by degree, editing label colors, add, delete, update skills

--- MvcCV/Controllers/YetenekController.cs
+++ MvcCV/Controllers/YetenekController.cs
@@ -17,5 +17,37 @@ namespace MvcCV.Controllers
             var yetenekler = repo.List();
             return View(yetenekler);
         }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek=repo.Find(x=>x.ID==id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekDuzenle(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekDuzenle(TblYeteneklerim t)
+        {
+            var y = repo.Find(x => x.ID == t.ID);
+            y.Yetenek = t.Yetenek;
+            y.Oran = t.Oran;
+            repo.TUpdate(y);
+            return RedirectToAction("Index");
+        }
     }
 }
