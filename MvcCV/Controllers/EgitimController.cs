6e08a4e3973c2c8b17deb9d01fafcf7ba7f6aef0
Melike İsbir <melikeisbr@gmail.com>
2023-08-30 00:08:40 +0300

    course list, adding education information

---
+++ MvcCV/Controllers/EgitimController.cs
@@ -0,0 +1,32 @@
+﻿using MvcCV.Models.Entity;
+using MvcCV.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCV.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitimlerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
