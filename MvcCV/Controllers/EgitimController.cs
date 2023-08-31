90a22b420fe1461c7eedadceac73d3b85a23a9b4
Melike İsbir <melikeisbr@gmail.com>
2023-08-31 20:30:01 +0300

    required validation control, max length validation control, education editing and deletion

--- MvcCV/Controllers/EgitimController.cs
+++ MvcCV/Controllers/EgitimController.cs
@@ -25,7 +25,40 @@ namespace MvcCV.Controllers
         [HttpPost]
         public ActionResult EgitimEkle(TblEgitimlerim p)
         {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
             repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim t)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBaslik1 = t.AltBaslik1;
+            egitim.AltBaslik2 = t.AltBaslik2;
+            egitim.GNO=t.GNO;
+            egitim.Tarih=t.Tarih;
+            repo.TUpdate(egitim);
             return RedirectToAction("Index");
         }
     }
