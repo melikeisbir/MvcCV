78a962c93218362d7bd5ef2e0870e7f452fc5f96
Melike İsbir <melikeisbr@gmail.com>
2023-09-01 21:37:30 +0300

    My Hobbies Page, Hobby Editing, About Me Page, Image Editing Process

--- MvcCV/Controllers/HakkimdaController.cs
+++ MvcCV/Controllers/HakkimdaController.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using MvcCV.Models.Entity;
+using MvcCV.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Repositories;
 
 namespace MvcCV.Controllers
 {
     public class HakkimdaController : Controller
     {
-        // GET: Hakkimda
+        GenericRepository<TblHakkimda> repo = new GenericRepository<TblHakkimda>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHakkimda p)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Ad = p.Ad;
+            t.Soyad = p.Soyad;
+            t.Adres = p.Adres;
+            t.Mail = p.Mail;
+            t.Telefon = p.Telefon;
+            t.Aciklama = p.Aciklama;
+            t.Resim = p.Resim;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
