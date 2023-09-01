83e1da7770e0625195ab3d2dbbc0460be2fe58ea
Melike İsbir <melikeisbr@gmail.com>
2023-09-01 22:18:47 +0300

    creating a social media table, partial social media

--- MvcCV/Controllers/DefaultController.cs
+++ MvcCV/Controllers/DefaultController.cs
@@ -16,11 +16,16 @@ namespace MvcCV.Controllers
             var degerler=db.TblHakkimda.ToList();
             return View(degerler);
         }
+        public PartialViewResult SosyalMedya()
+        {
+            var sosyalmedya = db.TblSosyalMedya.ToList();
+            return PartialView(sosyalmedya);
+        }
         public PartialViewResult Deneyim()
         {
             var deneyimler = db.TblDeneyimlerim.ToList();
             return PartialView(deneyimler);
-        } 
+        }
         public PartialViewResult Egitimlerim()
         {
             var egitimler = db.TblEgitimlerim.ToList();
