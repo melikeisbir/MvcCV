721deab0d4a59e4d64bbaef3b645fcd0e89a33ea
Melike İsbir <melikeisbr@gmail.com>
2023-09-04 23:08:18 +0300

    Login, Web Config Authorize Setting

--- MvcCV/Controllers/LoginController.cs
+++ MvcCV/Controllers/LoginController.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using MvcCV.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
 
 namespace MvcCV.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblAdmin p)
+        {
+            DbCVEntities db = new DbCVEntities();
+            var bilgi = db.TblAdmin.FirstOrDefault(x => x.KullaniciAdi == p.KullaniciAdi && x.Sifre == p.Sifre); 
+            if (bilgi != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.KullaniciAdi, false);
+                Session["KullaniciAdi"] = bilgi.KullaniciAdi.ToString();
+                return RedirectToAction("Index","Deneyim");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
     }
 }
