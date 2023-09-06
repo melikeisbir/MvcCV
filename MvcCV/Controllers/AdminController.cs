8a721ce1526c7a018dbe76cc71b3f82bef0f6e92
Melike İsbir <melikeisbr@gmail.com>
2023-09-06 20:16:29 +0300

    admin tasks, project finished

---
+++ MvcCV/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Repositories;
+
+namespace MvcCV.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        GenericRepository<TblAdmin> repo=new GenericRepository<TblAdmin>();
+        public ActionResult Index()
+        {
+            var liste = repo.List();
+            return View(liste);
+        }
+        [HttpGet]
+        public ActionResult AdminEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminEkle(TblAdmin p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult AdminSil(int id)
+        {
+            TblAdmin t = repo.Find(X => X.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AdminDuzenle(int id)
+        {
+            TblAdmin t = repo.Find(X => X.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult AdminDuzenle(TblAdmin p)
+        {
+            TblAdmin t = repo.Find(X => X.ID == p.ID);
+            t.KullaniciAdi = p.KullaniciAdi;
+            t.Sifre = p.Sifre;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
